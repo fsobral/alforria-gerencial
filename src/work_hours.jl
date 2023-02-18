3f51ac2ee8685b49d751ff07ba90f46f4bba3e7c
Francisco Nogueira Calmon Sobral <fncsobral@uem.br>
2023-02-17 22:14:40 -0300

    Add CSV to read csv files. Add new function.

--- src/work_hours.jl
+++ src/work_hours.jl
@@ -1,4 +1,27 @@
 using Dates
+using CSV: Rows
+
+function read_ch_files(directory, filename, professor)
+
+    wh = []
+
+    # Need to search all the directories having the format YYYYMMDD
+    for d in directory
+
+        # Need to search for the professor name in the CSV file
+        for row in CSV.Rows(d + filename, header=0,
+                            types=Dict(1=>String, 2=>Int, 3=>Int))
+
+            (row[1] == professor) &&
+                append!(wh, (d, Int(row[2]), Int(row[3])))
+
+        end
+
+    end
+
+    return wh
+
+end
 
 function work_hours(wh, dts::Date, dte::Date)
 
